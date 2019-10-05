65c03607a93c53cd64b363dacd3fa6cc5f310669
theSoenke <soenke.behrendt@gmail.com>
2019-10-05 11:27:37 +0200

    Add batch processing

--- head_pose.py
+++ head_pose.py
@@ -34,16 +34,18 @@ class HeadPose():
 
     @torch.no_grad()
     def predict(self, image):
-        if isinstance(image, list):
-            image = [self.transform(img) for img in image]
+        if isinstance(image, torch.Tensor) and len(image.shape) == 4:
+            data = torch.stack([self.transform(transforms.functional.to_pil_image(img)) for img in image])
+        if isinstance(image, torch.Tensor):
+            data = self.transform(transforms.functional.to_pil_image(image))
         elif isinstance(image, str):
             image = Image.open(image)
-            image = self.transform(image).unsqueeze(dim=0)
+            data = self.transform(image).unsqueeze(dim=0)
         else:
-            image = self.transform(image).unsqueeze(dim=0)
+            data = self.transform(image).unsqueeze(dim=0)
 
-        image = image.to(self.device)
-        yaw, pitch, roll = self.model(image)
+        data = data.to(self.device)
+        yaw, pitch, roll = self.model(data)
         yaw = F.softmax(yaw, dim=1)
         pitch = F.softmax(pitch, dim=1)
         roll = F.softmax(roll, dim=1)
@@ -51,7 +53,7 @@ class HeadPose():
         yaw = torch.sum(yaw * self.idx_tensor, dim=1) * 3 - 99
         pitch = torch.sum(pitch * self.idx_tensor, dim=1) * 3 - 99
         roll = torch.sum(roll * self.idx_tensor, dim=1) * 3 - 99
-        return yaw.item(), pitch.item(), roll.item()
+        return {'yaw': yaw, 'pitch': pitch, 'roll': roll}
 
 
 if __name__ == "__main__":
@@ -61,7 +63,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     head_pose = HeadPose(checkpoint=args.checkpoint)
-    yaw, pitch, roll = head_pose.predict(args.image)
+    angles = head_pose.predict(args.image)
+    yaw, pitch, roll = angles['yaw'].item(), angles['pitch'].item(), angles['roll'].item()
 
     print("Yaw: %f" % yaw)
     img = cv2.imread(args.image)
